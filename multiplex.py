9ab4ad597f924f70970f873674352100a2c71423
Damian Hamilton <damianhamilton25@hotmail.com>
2026-04-22 18:15:11 -0600

    added error handling

--- multiplex.py
+++ multiplex.py
@@ -2,15 +2,43 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+import os
 import sys
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget,
     QVBoxLayout, QHBoxLayout, QSlider,
-    QPushButton, QLabel, QFileDialog
+    QPushButton, QLabel, QFileDialog,
+    QMenu, QDialog, QDialogButtonBox
 )
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PySide6.QtMultimediaWidgets import QVideoWidget
-from PySide6.QtCore import Qt, QUrl
+from PySide6.QtCore import Qt, Signal, QUrl
+from PySide6.QtGui import QAction
+
+
+class ComplainDialog(QDialog):
+    def __init__(self, message):
+        super().__init__()
+
+        self.setWindowTitle("Oops!")
+
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok)
+        self.buttonBox.accepted.connect(self.accept)
+
+        layout = QVBoxLayout()
+        message_label = QLabel(message)
+        layout.addWidget(message_label)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
+    
+
+class ClickableVideoWidget(QVideoWidget):
+    clicked = Signal()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.clicked.emit()
+        super().mousePressEvent(event)
 
 
 class SimplePlayer(QMainWindow):
@@ -30,7 +58,7 @@ class SimplePlayer(QMainWindow):
         self.audio.setVolume(0.8)
 
         # video layer of the media widget
-        self.video_widget = QVideoWidget()
+        self.video_widget = ClickableVideoWidget()
         self.player.setVideoOutput(self.video_widget)
 
         # layout
@@ -42,35 +70,22 @@ class SimplePlayer(QMainWindow):
 
         layout.addWidget(self.video_widget, stretch=1)
 
-        # scrub bar
-        self.scrubber = QSlider(Qt.Horizontal)
-        self.scrubber.setRange(0, 0)
-        layout.addWidget(self.scrubber)
-
         # Controls row
         controls = QHBoxLayout()
         layout.addLayout(controls)
 
-        self.btn_open = QPushButton("Open File")
-        self.btn_play = QPushButton("Play")
-        self.btn_stop = QPushButton("Stop")
-        self.lbl_time = QLabel("0:00 / 0:00")
+        # scrub bar
+        self.scrubber = QSlider(Qt.Horizontal)
+        self.scrubber.setRange(0, 0)
+        controls.addWidget(self.scrubber)
 
-        controls.addWidget(self.btn_open)
-        controls.addWidget(self.btn_play)
-        controls.addWidget(self.btn_stop)
-        controls.addStretch()
+        self.lbl_time = QLabel("0:00 / 0:00")
         controls.addWidget(self.lbl_time)
 
         # -----Signals-----
 
-        # Buttons
-        self.btn_open.clicked.connect(self.open_file)
-        self.btn_play.clicked.connect(self.toggle_play)
-        self.btn_stop.clicked.connect(self.player.stop)
-
-        # state changes
-        self.player.playbackStateChanged.connect(self.on_state_changed)
+        # Player window
+        self.video_widget.clicked.connect(self.toggle_play)
 
         # position changes
         self.player.positionChanged.connect(self.on_position_changed)
@@ -83,15 +98,36 @@ class SimplePlayer(QMainWindow):
         if source:
             self.load(source)
 
+
+    def contextMenuEvent(self, e):
+        open_file_action = QAction("Open File", self)
+        open_file_action.triggered.connect(self.open_file)
+        open_url_action = QAction("Open URL", self)
+        open_url_action.triggered.connect(self.open_url)
+
+        context = QMenu(self)
+        context.addAction(open_file_action)
+        context.addAction(open_url_action)
+        context.exec(e.globalPos())
     
+
     # Helpers
+    def complain(self, message):
+        dlg = ComplainDialog(message)
+        dlg.exec()
+
+
     def load(self, source: str):
         """Accept either a local file path or a URI string"""
         if source.startswith(("http://", "https://", "rtsp://", "trmp://")):
             url = QUrl(source)
         else:
+            if not os.path.exists(source):
+                self.complain(f"The file '{source}' does not seem to exist.")
+                return
+            
             url = QUrl.fromLocalFile(source)
-
+        
         self.player.setSource(url)
         self.player.play()
 
@@ -105,6 +141,7 @@ class SimplePlayer(QMainWindow):
     
     # Slots
     def open_file(self):
+        """this one's for local video files on your machine"""
         path, _ = QFileDialog.getOpenFileName(
             self, "Open Video",
             filter="Video Files (*.mp4 *.mkv *.avi *.mov *.m3u8 *.ts);;All Files (*)"
@@ -113,21 +150,22 @@ class SimplePlayer(QMainWindow):
             self.load(path)
 
 
+    def open_url(self):
+        """this one's for digging a .m3u8 stream out of a webpage"""
+        pass
+
+
     def toggle_play(self):
+        """activated when left-clicking on the video surface"""
         if self.player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
             self.player.pause()
         else:
             self.player.play()
 
 
-    def on_state_changed(self, state):
-        if state == QMediaPlayer.PlaybackState.PlayingState:
-            self.btn_play.setText("Pause")
-        else:
-            self.btn_play.setText("Play")
-
-
     def on_position_changed(self, position_ms: int):
+        """activated when sliding the scrubber"""
+
         # blocks signals while we move the slider because 
         # apparently all this stuff is stupid and makes infinite loops
         self.scrubber.blockSignals(True)
@@ -142,11 +180,12 @@ class SimplePlayer(QMainWindow):
 
 
     def on_duration_changed(self, duration_ms: int):
+        """likely called only when stream source changes"""
         self.scrubber.setRange(0, duration_ms)
 
 
 def main():
-    logger.info("Doin stuff!")
+    logger.info("Starting the multiplex")
 
     # application instance
     app = QApplication(sys.argv)
